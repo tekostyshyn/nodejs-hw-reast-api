a9cbc6edb7c945bd9726134c01a20e8c9386bd1f
tekostyshyn <tekostyshyn@gmail.com>
2023-07-10 13:06:29 +0300

    Verification of email is added

--- models/user.js
+++ models/user.js
@@ -30,6 +30,14 @@ const userSchema = new Schema(
       type: String,
       required: true,
     },
+    verified: {
+      type: Boolean,
+      default: false,
+    },
+    verificationToken: {
+      type: String,
+      required: [true, "Verify token is required"],
+    },
   },
   { versionKey: false, timestamps: true }
 );
